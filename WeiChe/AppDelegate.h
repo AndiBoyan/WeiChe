09af7ba6bf045e36b4fca91a6f2ded9d8de26267
AndiBoyan <andi.boyan@qq.com>
2015-05-14 14:26:43 +0800

    Initial Commit

---
+++ WeiChe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiChe
+//
+//  Created by icePhoenix on 15/5/14.
+//  Copyright (c) 2015年 aodelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeiChe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeiChe
+//
+//  Created by icePhoenix on 15/5/14.
+//  Copyright (c) 2015年 aodelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
